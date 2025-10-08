7e2c7827aa928c71f0b59b68b5842ce8499b44e4
Canhadaz <murilofurlanetto266@gmail.com>
2025-10-08 00:52:36 -0300

    segundo commit (footer adicionado)

--- TCC/src/components/footer/index.tsx
+++ TCC/src/components/footer/index.tsx
@@ -3,14 +3,26 @@ import { getStyles } from "./styles"
 import { Feather, FontAwesome5 } from "@expo/vector-icons"
 import { Link } from "expo-router"
 
+import ifpr_footer from '../../../assets/images/ifpr_footer.png'
+
 const Footer = () => {
     const { width } = useWindowDimensions();
     const styles = getStyles(width);
 
     return (
-        <View  style={styles.main_container}>
-
+        <>
+        <View style={styles.green_status_bar}>
+            <Text style={styles.footer_text}>
+                Acesse projetos sustentáveis, independente de onde estiver!
+            </Text>
         </View>
+        <View  style={styles.main_container}>
+            <View style={styles.container_content}>
+            <Image source={ifpr_footer} style={styles.image}/>
+            <Text style={styles.h2_text}>Rua Emílio Bertolini, 54 - Cajuru, {'\n'}82920-030 Curitiba PR Brasil{'\n'}+55 (41) 3888-5250</Text>
+            </View>
+        </View>
+        </>
     )}
 
 export default Footer;
--- TCC/src/components/footer/styles.ts
+++ TCC/src/components/footer/styles.ts
@@ -7,9 +7,46 @@ export const getStyles = (width:number) => {
     
     return StyleSheet.create({
     main_container:{
-        backgroundColor: '#77A135',
-        height: 500,
+        backgroundColor: '#78a1356e',
+        height: 300,
         width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container_content:{
+        width: width>= 900 ? 900 : width >= 750 ? 550 : width >= 600 ? 400 : 300,
+        flexDirection:'column',
+        rowGap:30,
+        alignItems:'flex-start',
+        justifyContent:'flex-start',
+    },
+    h2_text:{
+        fontSize:18,
+        fontWeight: 500,
+        color: '#000000ff',
+        textAlign:'left',
+        marginLeft: 8,
+    },
+    footer_text:{
+        fontSize: width >= 620 ? 22 : 18,
+        fontWeight: 500,
+        color: 'white',
+        textAlign:'center',
+    },
+    green_status_bar:{
+        backgroundColor: '#77A135',
+        width: '100%',
+        marginTop: 40,
+        justifyContent:'center',
+        alignItems:'center',
+        padding: 3,
+        boxShadow: '0px 4px 8px #0000005c',
+    },
+    image:{
+        width: 288,
+        height: 103,
+        borderBottomWidth: 2,
+        borderBottomColor: '#7d7d7dff',
     },
 })
 }
